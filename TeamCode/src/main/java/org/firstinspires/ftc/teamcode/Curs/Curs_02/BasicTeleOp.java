49262fd85fec86b6f7e29dfc996c448e67b75a8d
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-10-05 18:07:59 +0300

    Update BasicTeleOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Curs/Curs_02/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Curs/Curs_02/BasicTeleOp.java
@@ -82,9 +82,7 @@ public class BasicTeleOp extends LinearOpMode {
 
             leftMotor.setPower(drive + turn);
             rightMotor.setPower(drive + turn);
-
-
-            telemetry.addData("Status", "Running");
+            
             telemetry.update();
         }
     }
